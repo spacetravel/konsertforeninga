6e64a726256081ec62d243647afbbe102a62b10b
Spacetravel Øyvind <spacetravel.no@gmail.com>
2012-11-26 20:52:02 +0100

    Testing

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ Konsertforeninga::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
@@ -48,7 +48,6 @@ Konsertforeninga::Application.configure do
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
   config.assets.precompile += %w[active_admin.css active_admin.js]
-  config.assets.compile = true
   
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
