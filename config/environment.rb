87a356ed86dc4c133817af50d0992f3d9d72571b
Spacetravel Øyvind <spacetravel.no@gmail.com>
2012-11-28 20:18:13 +0100

    fixed amazon storage 5

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
-AWS::S3::DEFAULT_HOST = "s3-eu-west-1.amazonaws.com"
-
 # Initialize the rails application
 Konsertforeninga::Application.initialize!
+
+AWS::S3::DEFAULT_HOST = "s3-eu-west-1.amazonaws.com"
