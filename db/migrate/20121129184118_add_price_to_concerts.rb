f9e37c8d6aaf0f7831b926715c4bd5a2808227cf
Spacetravel Øyvind <spacetravel.no@gmail.com>
2012-11-29 19:42:23 +0100

    fixed price in concerts

--- db/migrate/20121129184118_add_price_to_concerts.rb
+++ db/migrate/20121129184118_add_price_to_concerts.rb
@@ -1,9 +1,6 @@
-class ChangePriceInConcerts < ActiveRecord::Migration
-  def up
+class AddPriceToConcerts < ActiveRecord::Migration
+  def change
     remove_column :concerts, :price
     add_column :concerts, :price, :string
-  end
-
-  def down
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121129173046) do
+ActiveRecord::Schema.define(:version => 20121129184118) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -50,7 +50,6 @@ ActiveRecord::Schema.define(:version => 20121129173046) do
     t.string   "title"
     t.text     "desc"
     t.integer  "serie_id"
-    t.integer  "price"
     t.datetime "created_at",         :null => false
     t.datetime "updated_at",         :null => false
     t.integer  "venue_id"
@@ -59,6 +58,7 @@ ActiveRecord::Schema.define(:version => 20121129173046) do
     t.string   "photo_file_size"
     t.date     "show_date"
     t.string   "show_time"
+    t.string   "price"
   end
 
   create_table "news_bulletins", :force => true do |t|
