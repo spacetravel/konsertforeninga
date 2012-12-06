6370bafe601379c5d60a503c6b2c82f1ab2b6177
Spacetravel Øyvind <spacetravel.no@gmail.com>
2012-12-06 20:46:02 +0100

    fixed series view

--- app/controllers/konsertserier_controller.rb
+++ app/controllers/konsertserier_controller.rb
@@ -6,6 +6,8 @@ class KonsertserierController < ApplicationController
   
   def show
     @serie = Serie.find(params[:id])
-    @konserter = Concert.where("serie_id = ?",params[:id]).order('show_date DESC')
+    @konserter_past = Concert.where("show_date < ? AND serie_id = ?",Time.now, params[:id]).order('show_date DESC')
+
+    @konserter_future = Concert.where("show_date >= ? AND serie_id = ?",Time.now, params[:id]).order('show_date DESC')
   end
 end
