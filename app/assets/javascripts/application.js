ed362b63011704ee77f55adef64ad546d1512f07
Spacetravel Øyvind Dahl <spacetravel.no@gmail.com>
2013-01-28 14:51:55 +0100

    final images

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,10 +10,11 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//= require jquery.lightbox-0.5
-//= require carousel
+
 //= require jquery
 //= require jquery_ujs
+//= require jquery.lightbox-0.5
+//= require carousel
 //= require_tree .
 
 $(document).ready(function() {
